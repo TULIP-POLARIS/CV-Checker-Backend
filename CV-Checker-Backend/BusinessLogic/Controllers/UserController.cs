5da991c11eaec61df3751476663306f92543b011
Diego Barreira <contact.diegobarreira@gmail.com>
2026-03-02 15:11:21 +0100

    USER Controller SAVED

---
+++ CV-Checker-Backend/BusinessLogic/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLogic.Entities;
+using BusinessLogic;
+
+namespace CVApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public UserController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // Create a new user
+        [HttpPost]
+        public JsonResult CreateUser(User user)
+            {
+            var newUser = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = user.Name,
+                Email = user.Email,
+                PasswordHash = user.PasswordHash
+            };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+            return new JsonResult(Ok(newUser));
+        }
+    }
+}
+
--- CV-Checker-Backend/BusinessLogic/Program.cs
+++ CV-Checker-Backend/BusinessLogic/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using BusinessLogic;
+using CVApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
