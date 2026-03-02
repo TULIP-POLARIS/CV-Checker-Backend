cf8f8ef9bea79b8e8c957dccc3a1f4753a77926b
Diego Barreira <contact.diegobarreira@gmail.com>
2026-03-02 14:21:13 +0100

    NuGet Packages Installed + ApiContext Updated

--- CV-Checker-Backend/BusinessLogic/ApiContext.cs
+++ CV-Checker-Backend/BusinessLogic/ApiContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BusinessLogic.Entities;
+using Microsoft.EntityFrameworkCore;
+using BusinessLogic.Entities;
 
 namespace BusinessLogic
 {
-    public class ApiContext
+    public class ApiContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+        }
     }
 }
