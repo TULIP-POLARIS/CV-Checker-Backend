be53b4e666b78cc51c17c2abb38cc3a1b36cb457
i549083 <i549083@fontysict.nl>
2026-03-03 10:10:42 +0100

    fiexed versions AGAIN

--- CV-Checker-Backend/Domain/Entities/User.cs
+++ CV-Checker-Backend/Domain/Entities/User.cs
@@ -3,13 +3,13 @@
     {
         public Guid Id { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         public string? PhoneNumber { get; set; }
 
-        public string PasswordHash { get; set; }
+        public string? PasswordHash { get; set; }
 
         public DateTime CreatedAt { get; set; }
     }
