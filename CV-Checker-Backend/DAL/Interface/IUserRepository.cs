0778e694c7f2025c7678becde3b3f64d939e9dad
Diego Barreira <contact.diegobarreira@gmail.com>
2026-03-03 10:00:23 +0100

    FIX

--- CV-Checker-Backend/DAL/Interface/IUserRepository.cs
+++ CV-Checker-Backend/DAL/Interface/IUserRepository.cs
@@ -8,7 +8,7 @@ namespace DAL.Interface
 {
     public interface IUserRepository
     {
-        Task<User> GetByIdAsync(int id);
+        Task<User> GetByIdAsync(Guid id);
         Task<User> GetByEmailAsync(string email);
         Task<User> GetByPhoneAsync(string phoneNumber);
         Task<User> CreateUserAsync(User user);
